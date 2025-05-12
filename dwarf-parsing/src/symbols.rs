8b4cee7d3078677982409cbce240db1b4625055d
ELginas <gintaras.z123@yahoo.com>
2025-05-12 20:00:46 +0300

    fix: handle non mangled names

--- dwarf-parsing/src/symbols.rs
+++ dwarf-parsing/src/symbols.rs
@@ -7,14 +7,19 @@ pub fn run(path: &str) {
     let mmap = unsafe { memmap2::Mmap::map(&file).unwrap() };
     let object = object::File::parse(&*mmap).unwrap();
 
-    let address = 53688592;
+    let address = 52197888;
 
     for symbol in object.symbols() {
         if address != symbol.address() {
             continue;
         }
         let name = symbol.name().unwrap();
-        let sym = Symbol::new(name).unwrap();
+        let sym = Symbol::new(name);
+        if let Err(_) = sym {
+            println!("no demangling: {} {}: {:x?}", symbol.index(), name, symbol);
+            continue;
+        }
+        let sym = sym.unwrap();
         let name = sym.to_string();
         println!("{} - {}: {:x?}", symbol.index(), name, symbol);
     }
